b579305b048b64e733cdeaef5bc54b4cce2f7529
Linkon Siddique <linkon.sq48@gmail.com>
2020-09-11 00:06:23 +0600

    Initial

---
+++ android/app/src/main/kotlin/com/linkonsiddique/google_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.linkonsiddique.google_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_map/screens/map_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MapScreen(),
+    );
+  }
+}
